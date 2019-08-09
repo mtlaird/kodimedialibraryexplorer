e24e31e6f248d1f9058691df35b51f93c9efc4cf
Matthew <mtlaird@gmail.com>
2019-08-08 22:38:27 -0400

    Make config file location settable at runtime

--- run_webserver.py
+++ run_webserver.py
@@ -1,14 +1,25 @@
+import argparse
 import os
 from KodiMediaLibraryExplorer import app
 import json
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config-file", "-c")
+
+    args = parser.parse_args()
+    clean_args = {k: v for k, v in vars(args).items() if v is not None}
+    confg_file = clean_args["config_file"] if "config_file" in clean_args else "config.json"
+
     try:
         with open("config.json") as f:
             config = json.load(f)
             webserver_host = config['webserver-host']
+    except FileNotFoundError:
+        raise
     except IOError:
-        webserver_host = '127.0.0.1'
+        raise
 
     app.root_path = os.getcwd()
     app.run(host=webserver_host, port=5050)
