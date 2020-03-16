2cea5dd8b8bdfa7a829423dfef44b9f9ea19cf94
Matthew <mtlaird@gmail.com>
2020-03-15 23:49:49 -0400

    Configure taglist root

--- KodiTaglist.py
+++ KodiTaglist.py
@@ -1,21 +1,25 @@
 import csv
 import json
 from os import listdir
+from flask import current_app
 
 
 class Taglist:
     def __init__(self, list_name):
+        taglist_root = current_app.config["taglist_root"]
+        if taglist_root != '' and taglist_root[-1] != '/':
+            taglist_root += '/'
         if list_name.endswith(".json"):
-            with open('taglists_meta/' + list_name) as f:
+            with open(taglist_root + 'taglists_meta/' + list_name) as f:
                 self.metadata = json.load(f)
                 self.name = list_name.split(".json")[0]
         else:
-            with open('taglists_meta/' + list_name + '.json') as f:
+            with open(taglist_root + 'taglists_meta/' + list_name + '.json') as f:
                 self.metadata = json.load(f)
                 self.name = list_name
 
         self.movies = []
-        with open('taglists_data/' + self.metadata['source']) as f:
+        with open(taglist_root + 'taglists_data/' + self.metadata['source']) as f:
             reader = csv.DictReader(f)
             for row in reader:
                 self.movies.append(row)
@@ -27,7 +31,10 @@ class Taglist:
 
 class TaglistDirectory:
     def __init__(self):
+        taglist_root = current_app.config["taglist_root"]
+        if taglist_root != '' and taglist_root[-1] != '/':
+            taglist_root += '/'
         self.taglists = {}
-        for f in listdir("taglists_meta"):
+        for f in listdir(taglist_root + "taglists_meta"):
             temp = Taglist(f)
             self.taglists[temp.metadata['name']] = temp
